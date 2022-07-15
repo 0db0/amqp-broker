400a3d34a934312937166d436e9bdac2abbd47cd
Dmitry <ferre1812@gmail.com>
2022-07-15 18:17:20 +0400

    done exchanges case

--- src/Producer/producer.php
+++ src/Producer/producer.php
@@ -21,20 +21,10 @@ $connection = new AMQPStreamConnection('message-broker', 5672, 'guest', 'guest')
 $channel = $connection->channel();
 $channel->exchange_declare('logs', AMQPExchangeType::FANOUT, false, false, false);
 
-[$queueName] = $channel->queue_declare('', false, false, true, false);
-
-$channel->exchange_bind($queueName, 'logs');
-
 $message= new AMQPMessage($content);
-$channel->basic_publish($message, '', 'hello_queue');
+$channel->basic_publish($message, 'logs');
 
 echo sprintf('[x] Sent %s%s', $content, PHP_EOL);
 
-$shutdownCallback = static function(AMQPChannel $channel, AMQPStreamConnection $connection): void {
-    $channel->close();
-    $connection->close();
-};
-
-register_shutdown_function($shutdownCallback, $channel, $connection);
-
-$channel->consume();
+$channel->close();
+$connection->close();
