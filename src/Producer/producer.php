0216c018798b86f4430ced6a917ba5e8f3403a7e
Dmitry <ferre1812@gmail.com>
2022-07-15 17:36:58 +0400

    add exchanges declaration

--- src/Producer/producer.php
+++ src/Producer/producer.php
@@ -6,6 +6,8 @@ require __DIR__ . '/../../vendor/autoload.php';
 
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
+use PhpAmqpLib\Channel\AMQPChannel;
+use PhpAmqpLib\Exchange\AMQPExchangeType;
 
 error_reporting(E_ALL & ~E_DEPRECATED);
 
@@ -17,12 +19,22 @@ if ($argc > 1) {
 
 $connection = new AMQPStreamConnection('message-broker', 5672, 'guest', 'guest');
 $channel = $connection->channel();
-$channel->queue_declare('hello_queue', false, false, false, false);
+$channel->exchange_declare('logs', AMQPExchangeType::FANOUT, false, false, false);
+
+[$queueName] = $channel->queue_declare('', false, false, true, false);
+
+$channel->exchange_bind($queueName, 'logs');
 
 $message= new AMQPMessage($content);
 $channel->basic_publish($message, '', 'hello_queue');
 
 echo sprintf('[x] Sent %s%s', $content, PHP_EOL);
 
-$channel->close();
-$connection->close();
+$shutdownCallback = static function(AMQPChannel $channel, AMQPStreamConnection $connection): void {
+    $channel->close();
+    $connection->close();
+};
+
+register_shutdown_function($shutdownCallback, $channel, $connection);
+
+$channel->consume();
